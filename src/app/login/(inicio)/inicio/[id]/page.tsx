da037bc36a1aaabd022603abfc4212c617aeb235
Gustavo-Teotonio <gustavohidroar07@gmail.com>
2025-10-15 21:30:40 -0300

    corrigindo o vercel

--- src/app/login/(inicio)/inicio/[id]/page.tsx
+++ src/app/login/(inicio)/inicio/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Modal } from "@/components/Modal";
-import { loginSchema, TLoginSchema } from "@/schemas/loginSchema";
+import { loginSchema, TLoginSchema } from "../../../../../schemas/loginSchema";
 import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
